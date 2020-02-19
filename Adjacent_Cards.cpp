d74fece19755d3156673391f33dc4f626a9b6d5e
Leverkuhn <34468780+Leverkuhn@users.noreply.github.com>
2020-02-19 15:14:57 -0800

    Add files via upload

--- Adjacent_Cards.cpp
+++ Adjacent_Cards.cpp
@@ -1,6 +1,3 @@
-// Adjacent_Cards.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// If we deal a standard deck in a circle, what is the expected number of adjacent pairs of black cards? Let's determine this experimentally.
-
 #include <iostream>     // std::cout
 #include <algorithm>    // std::shuffle
 #include <array>        // std::array
@@ -8,7 +5,18 @@
 #include <chrono>       // std::chrono::system_clock
 
 int main() {
-	std::array<int, 5> foo{ 1,2,3,4,5 };
+  // Ask for the size of the deck to be dealt.
+  int deckSize;
+  std::cout << "Enter the deck size: ";
+  std::cin >> deckSize;
+  std::cin.ignore( 256, '\n');
+  // Ask for number of tests
+  std::cout << "Enter the number of test to run: ";
+  int numTests;
+  std::cin >> numTests;
+
+  // Generate the deck.
+	std::array<int, 4> foo{ 1 , 1, 2, 2};
 
 	// obtain a time-based seed:
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
@@ -18,6 +26,19 @@ int main() {
 	std::cout << "shuffled elements:";
 	for (int& x : foo) std::cout << ' ' << x;
 	std::cout << '\n';
+
+  // Count the number of adjacents 1s:
+  int adjCount = 0; 
+  for (int i{ 0}; i < 3; i++)
+    if (foo[i] == 1 && foo[i + 1] == 1)
+    {
+      adjCount++;
+    }
+  if (foo[0] == 1 && foo [3] == 1) // check if the first and last elements are matches
+  {
+    adjCount++;
+  }
+  std::cout << "There are " << adjCount << " adjacent 1s \n";
 
 	return 0;
 }
